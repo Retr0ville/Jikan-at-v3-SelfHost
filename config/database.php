088fa1e5cfe145dd55ddf404402a08ba10e649ed
Samuel Akhaze <Xclusivesam14@gmail.com>
2023-09-16 21:20:23 +0100

    Update database.php

--- config/database.php
+++ config/database.php
@@ -3,6 +3,7 @@
 return [
     'redis' => [
         'client' => 'predis',
-        'url' => 'redis://red-ck2vptvqj8ts7395hjug:6379?database=0'
+        'default' => 
+    ['url' => 'redis://red-ck2vptvqj8ts7395hjug:6379?database=0']
     ]
 ];
